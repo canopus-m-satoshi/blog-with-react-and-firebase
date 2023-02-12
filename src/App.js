ecf8b55189b01f0bce65784146872e7afb0ce852
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-12 12:18:59 +0900

    config react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import CreatePost from './components/CreatePost'
+import Home from './components/Home'
+import Login from './components/Login'
+import Logout from './components/Logout'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
