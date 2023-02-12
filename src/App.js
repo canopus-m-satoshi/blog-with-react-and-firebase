49decdb3885ffb85f1cc260a3485f5a726bb55cc
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-12 18:44:51 +0900

    feature login with google

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import CreatePost from './components/CreatePost'
 import Home from './components/Home'
@@ -6,13 +7,15 @@ import Logout from './components/Logout'
 import Navbar from './components/Navbar'
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/createpost" element={<CreatePost />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         <Route path="/logout" element={<Logout />} />
       </Routes>
     </Router>
