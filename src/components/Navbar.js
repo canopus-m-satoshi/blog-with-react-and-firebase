1ba8e84a5e8fa34c4fca4090d9e1dc504fa730a1
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-12 18:17:40 +0900

    installed fontawesome and set icons in nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
     <nav className="bg-yellow-600 text-white flex items-center justify-center gap-4 p-2">
       <Link to="/" className="text-white hover:text-green-500">
+        <FontAwesomeIcon icon={faHouse} className="pr-2" />
         Home
       </Link>
       <Link to="/createpost" className="text-white hover:text-green-500">
+        <FontAwesomeIcon icon={faFilePen} className="pr-2" />
         Create Post
       </Link>
       <Link to="/login" className="text-white hover:text-green-500">
+        <FontAwesomeIcon icon={faArrowRightToBracket} className="pr-2" />
         Login
       </Link>
     </nav>
