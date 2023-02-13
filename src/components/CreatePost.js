95a2b7c4c57efd877fadb5db0b663d7837da447d
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-13 22:25:45 +0900

    styling

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,43 @@
+import FormControl from '@mui/material/FormControl'
+import Input from '@mui/material/Input'
+import InputLabel from '@mui/material/InputLabel'
+import TextareaAutosize from '@mui/base/TextareaAutosize'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  return (
+    <Container>
+      <Box
+        sx={{
+          width: 600,
+          height: 400,
+          my: 10,
+          mx: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 3,
+          bgcolor: 'background.level1',
+        }}>
+        <h1>Post article</h1>
+        <div>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="component-helper">Articl Title</InputLabel>
+            <Input id="component-helper" />
+          </FormControl>
+        </div>
+        <div>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={3}
+            placeholder="Write down contents"
+          />
+        </div>
+        <Button variant="contained">Post</Button>
+      </Box>
+    </Container>
+  )
 }
 export default CreatePost
